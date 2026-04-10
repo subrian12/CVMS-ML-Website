a76d741f72707f07a44edb9f2cc14e6a86ba792c
Brian Su <bsu92130@gmail.com>
2026-04-09 22:25:47 -0700

    ...

--- js/home.js
+++ js/home.js
@@ -1,7 +1,13 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const navbar = document.querySelector('.navbar');
+
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
+    navbar.classList.toggle('menu-open');
 }) 
+
+
+
